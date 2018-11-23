1fb53403215b299b5d8d4431da860159404dfcc4
Michael Perret <michael.perret@epfl.ch>
2018-11-23 16:57:25 +0100

    AP working. Wrong id implem. and improve SP_landing to do

--- build/waypoint_navigation/catkin_generated/pkg.develspace.context.pc.py
+++ build/waypoint_navigation/catkin_generated/pkg.develspace.context.pc.py
@@ -2,7 +2,7 @@
 CATKIN_PACKAGE_PREFIX = ""
 PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
 PROJECT_CATKIN_DEPENDS = "message_runtime".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lapriltag_detector".split(';') if "-lapriltag_detector" != "" else []
 PROJECT_NAME = "waypoint_navigation"
 PROJECT_SPACE_DIR = "/home/mpperret/project_ws/devel"
 PROJECT_VERSION = "0.0.0"
--- src/waypoint_navigation/src/offb_node.cpp
+++ src/waypoint_navigation/src/offb_node.cpp
@@ -11,6 +11,7 @@
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/Sparse> 
 using namespace Eigen;
+//namespace apriltags_ros;
 
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>      // Mavros local pose
@@ -20,6 +21,7 @@ using namespace Eigen;
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
 #include <gazebo_msgs/ModelStates.h>
+#include <apriltags_ros/AprilTagDetectionArray.h>
 
 
 #include "std_msgs/String.h"
@@ -29,25 +31,48 @@ using namespace Eigen;
 
 #include "offb_node.h"
 
-// From Aerial robots
-#define POS_ACCEPT_RAD 0.2f
+#define DESIRED_ID 2
+#define TOL 0.1f
+#define POS_ACCEPT_RAD 0.2f // From Aerial robots
+#define AP_SIZE 0.2f
+#define AP_POS_ACCEPT 0.1f
+#define AP_POS_ACCEPT_X 0.1f
+#define AP_POS_ACCEPT_Y 0.1f
+#define HEIGHT 1.5f
+#define OFFSET_CAM_X 0
+#define OFFSET_CAM_Y 0
+#define SP_SIZE_WIDTH 0.8f  // SP=Solar Panel of size 1.6x0.8[m]
+#define SP_SIZE_LENGTH 1.6f
+#define SP_SIZE_HEIGHT 0.8f
 
 
 // Global variables
-int n_AP=0; //To know how many AP are detected
+int idx  = 0; // index to select a point in the list of points 
+int n_AP = 0; //To know how many AP are detected
+int n_model=0;
+int AP_id=10;
+
+bool armed = true;
+bool traj_done = false;
+bool AP_detected  = false;
+bool AP_centered  = false;
+bool AP_verified  = false;
+bool takeoff_done = false;
+bool landing_done = false;
+bool landing_in_progress = false;
+
+
+
 mavros_msgs::State          current_state;
 geometry_msgs::PoseStamped  est_local_pos;      // Mavros local pose
 gazebo_msgs::ModelStates    true_local_pos;     // Gazebo true local pose
-geometry_msgs::PoseArray    APtag_est_pos;      // Tag detection
-
+//geometry_msgs::PoseArray    APtag_est_pos;      // Tag detection
+apriltags_ros::AprilTagDetectionArray APtag_est_pos;// Tag detection
 
 
 int main(int argc, char **argv)
 {
 
-    int  next_pose = 0;
-    bool armed = true;
-    bool traj_done = false;
 
     // Initialize ROS => where we specify the name of our node
     ros::init(argc, argv, "offb_node");
@@ -66,8 +91,11 @@ ros::Subscriber true_local_pos_sub = nh.subscribe<gazebo_msgs::ModelStates>
 ("gazebo/model_states", 10, true_local_pos_cb);
 
 // The node subscribe to Topic "/tag_detections_pose", 10 msgs in buffer before deleting
-ros::Subscriber APtag_est_pos_sub = nh.subscribe<geometry_msgs::PoseArray>
-("tag_detections_pose", 10, APtag_est_pos_cb);
+//ros::Subscriber APtag_est_pos_sub = nh.subscribe<geometry_msgs::PoseArray>
+//("tag_detections_pose", 10, APtag_est_pos_cb);
+// The node subscribe to Topic "/tag_detections_pose", 10 msgs in buffer before deleting
+ros::Subscriber APtag_est_pos_sub = nh.subscribe<apriltags_ros::AprilTagDetectionArray>
+("tag_detections", 10, APtag_est_pos_cb);
 
     // The node publish the commanded local position        
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
@@ -90,29 +118,39 @@ ros::Subscriber APtag_est_pos_sub = nh.subscribe<geometry_msgs::PoseArray>
         rate.sleep();
     }
 
-    Vector3f pos      (0.0f, 0.0f, 0.0f);
-    Vector3f appos    (0.0f, 0.0f, 0.0f);
-    Vector3f goal_pos (0.0f, 0.0f, 0.0f);
+
+    Vector3f pos        (0.0f, 0.0f, 0.0f);
+    Vector3f AP_pos     (0.0f, 0.0f, 0.0f);
+    Vector3f AP_pos_save(0.0f, 0.0f, 0.0f);
+    Vector3f goal_pos   (5.0f, 5.0f, 5.0f);
 
     // Waypoint navigation list
-    Vector3f pos0( 0.0f,  0.0f, 2.0f);
-    Vector3f pos1( 2.0f,  2.0f, 2.0f);
-    Vector3f pos2( 2.0f, -2.0f, 2.0f);
-    Vector3f pos3(-2.0f, -2.0f, 2.0f);
-    Vector3f pos4(-2.0f,  2.0f, 2.0f);
-    Vector3f pos5( 0.0f,  0.0f, 1.0f);
-    Vector3f pos6( 0.0f,  0.0f, 0.5f);
-    Vector3f pos7( 0.0f,  0.0f, 0.0f);
+    Vector3f takeoff( 0.0f,  0.0f, HEIGHT);
+    // To ensure there is no problem for landing, you must set more waypoint than for landing
+    // Verify this
+    Vector3f pos0( 2.0f,  2.0f, HEIGHT);
+    Vector3f pos1( 2.0f, -2.0f, HEIGHT);
+    Vector3f pos2(-2.0f, -2.0f, HEIGHT);
+    Vector3f pos3(-2.0f,  2.0f, HEIGHT);
 
-    //Vector3f waypoints[8] = {
-    //{ 0.0f,  0.0f, 2.0f},
-    //{ 2.0f,  2.0f, 2.0f},
-    //{ 2.0f, -2.0f, 2.0f},
-    //{-2.0f, -2.0f, 2.0f},
-    //{-2.0f,  2.0f, 2.0f},
-    //{ 0.0f,  0.0f, 1.0f},
-    //{ 0.0f,  0.0f, 0.5f},
-    //{ 0.0f,  0.0f, 0.0f}};
+    Vector3f landing1( 0.0f,  0.0f, HEIGHT/2.0f);
+    Vector3f landing2( 0.0f,  0.0f, HEIGHT/5.0f);
+    Vector3f landing3( 0.0f,  0.0f, 0.0f);
+
+
+    Vector3f waypoints[4] = {pos0, pos1, pos2, pos3};
+    Vector3f   landing[3] = {landing1, landing2, landing3};
+    
+    int size_wp   = sizeof(waypoints)/sizeof(waypoints[0]);
+    int size_land = sizeof(landing)/sizeof(landing[0]);
+
+    
+    //send a few setpoints before starting
+    for(int i = 50; ros::ok() && i > 0; --i){
+        local_pos_pub.publish(conversion_to_msg(pos));
+        ros::spinOnce();
+        rate.sleep();
+    }
 
     // Create msgs_structure of the type mavros_msgs::SetMode
     mavros_msgs::SetMode offb_set_mode;
@@ -128,102 +166,109 @@ ros::Subscriber APtag_est_pos_sub = nh.subscribe<geometry_msgs::PoseArray>
 
 
     while(ros::ok()){
-        if( current_state.mode != "OFFBOARD" &&
-            (ros::Time::now() - last_request > ros::Duration(5.0)))
-        {
-            if( set_mode_client.call(offb_set_mode) &&
-                offb_set_mode.response.mode_sent)
-            {
+        if( current_state.mode != "OFFBOARD" && (ros::Time::now() - last_request > ros::Duration(5.0))){
+            if( set_mode_client.call(offb_set_mode) && offb_set_mode.response.mode_sent)
                 ROS_INFO("Offboard enabled");
-            }
             last_request = ros::Time::now();
         } 
-        else 
-        {
-            if( !current_state.armed &&
-                (ros::Time::now() - last_request > ros::Duration(5.0)))
-            {
-                if( arming_client.call(arm_cmd) &&
-                    arm_cmd.response.success)
-                {
+        else{
+            if( !current_state.armed && (ros::Time::now() - last_request > ros::Duration(5.0))){
+                if( arming_client.call(arm_cmd) && arm_cmd.response.success)
                     ROS_INFO("Vehicle armed");
-                }
                 last_request = ros::Time::now();
             }
         }
 
+
         // Current location in vector form.
         pos = conversion_to_vect(est_local_pos);
-        //ROS_INFO("Pos[%f,%f,%f]", pos(0),pos(1),pos(2));
+        //ROS_INFO("Pos =[%f, %f, %f], idx=%d", pos(0),pos(1),pos(2),idx);
 
-        // Choo/*se the next waypoint
-        switch(next_pose){
-            case 0 : goal_pos = pos0; break;
-            case 1 : goal_pos = pos1; break;
-            case 2 : goal_pos = pos2; break;
-            case 3 : goal_pos = pos3; break;
-            case 4 : goal_pos = pos4; break;
-            case 5 : goal_pos = pos5; break;
-            case 6 : goal_pos = pos6; break;
-            case 7 : goal_pos = pos7; break;
-            default : goal_pos = goal_pos; break;
-        }
+        
+        if(AP_detected && !AP_verified){
+            //AP_pos(0) = APtag_est_pos.poses[0].position.x;
+            //AP_pos(1) = APtag_est_pos.poses[0].position.y;
+            //AP_pos(2) = HEIGHT;
+            AP_pos(0) = APtag_est_pos.detections[0].pose.pose.position.x;
+            AP_pos(1) = APtag_est_pos.detections[0].pose.pose.position.y;
+            AP_pos(2) = HEIGHT;
+            //AP_pos(2) = APtag_est_pos.poses[0].position.z;
 
-        if( arming_client.call(arm_cmd) &&  is_goal_reached(goal_pos, pos, POS_ACCEPT_RAD) )
-        {
-            Vector3f v;
-            v(0) = true_local_pos.pose[2].position.x;
-            v(1) = true_local_pos.pose[2].position.y;
-            v(2) = true_local_pos.pose[2].position.z;
-            //ROS_INFO("pos_GOAL=[%f, %f, %f]", v(0), v(1), v(2));
-            
-            // Current APtag location in vector form.
-            //Vector3f appos;
-            //appos(0) = APtag_est_pos.poses[2].position.x;
-            //appos(1) = APtag_est_pos.poses[2].position.y;
-            //appos(2) = APtag_est_pos.poses[2].position.z;
-            //ROS_INFO("AP_Pos[%f,%f,%f]", appos(0),appos(1),appos(2));
-            
-            if( traj_done && next_pose == 0 )
-                next_pose = 5;
-            else if (traj_done && next_pose>= 5 && next_pose < 7)
-                next_pose += 1;
-            else if (next_pose == 4)
-            {
-                //ROS_INFO("Boucle effectuee");
-                traj_done = true;
-                next_pose = 0; // go to the middle
-            } 
-            else if (next_pose == 7)
-                current_state.armed = false;
-            else 
-            {
-                next_pose += 1;
-                
-                //Vector3f v;
-                //v(0) = true_local_pos.pose[2].position.x;
-                //v(1) = true_local_pos.pose[2].position.y;
-                //v(2) = true_local_pos.pose[2].position.z;
-                //ROS_INFO("pos_iteration=[%f, %f, %f]", v(0), v(1), v(2));
-            }    
-            
-            // We publish the estimated position
-            //ROS_INFO("Boucle en cours");    
+            goal_pos = to_center_pose(pos, AP_pos, OFFSET_CAM_X, OFFSET_CAM_Y);
+
+            //ROS_INFO("AP  =[%f, %f, %f]", AP_pos(0),AP_pos(1),AP_pos(2));
+            //ROS_INFO("Pos =[%f, %f, %f]", pos(0),pos(1),pos(2));
+            //ROS_INFO("Goal=[%f, %f, %f]", goal_pos(0),goal_pos(1),goal_pos(2));
+            //if(n_model>0)
+            //ROS_INFO("TLP =[%f, %f, %f]", true_local_pos.pose[5].position.x,true_local_pos.pose[5].position.y,true_local_pos.pose[5].position.z);
+
+            if(is_AP_centered(AP_pos, AP_POS_ACCEPT_X, AP_POS_ACCEPT_Y)){
+                //ROS_INFO("YEAH YOUR CENTERED");
+                if(check_id(DESIRED_ID)){
+                    ROS_INFO("ID IS GOOD");
+                    AP_verified = true;
+                    AP_pos_save = goal_pos;                  
+                    // Go to landing
+                }
+                else
+                    ; //ROS_INFO("ID IS WRONG");
+            }
             local_pos_pub.publish(conversion_to_msg(goal_pos));
         }
         else
         {
-            if (!current_state.armed && traj_done){
+            if(arming_client.call(arm_cmd) && is_goal_reached(goal_pos, pos, POS_ACCEPT_RAD)){
+                if(AP_verified) ;
+                else if(idx==0 && !takeoff_done){
+                    takeoff_done = true;
+                    idx = 0; // To reset for waypoints array
+                }
+                else if(takeoff_done && idx == (size_wp-1)){
+                    traj_done = true;
+                    idx = 0;
+                }
+                else if(takeoff_done && traj_done && idx == (size_land-1)){
+                    landing_done = true;
+                    current_state.armed = false;
+                }
+                else if( takeoff_done && traj_done && landing_done)
+                    idx = idx;
+                else
+                    idx++;
+            }
+                
+
+
+
+            if(!current_state.armed && landing_done){
                 arm_cmd.request.value = false;
                 // Disarmed drone
-                if( !arming_client.call(arm_cmd) &&
-                    !arm_cmd.response.success)
-                {
+                if( !arming_client.call(arm_cmd) && !arm_cmd.response.success)
                     ROS_INFO("Vehicle disarmed");
-                }
             }
-            else
+            else{   
+                if(takeoff_done){
+                    if(AP_verified){
+                        if(!landing_in_progress)
+                            goal_pos = landing_on_SP(AP_pos_save, AP_id);
+                        if(is_goal_reached(goal_pos, pos, POS_ACCEPT_RAD)){
+                             landing_in_progress = true;
+                             goal_pos = lands(pos, SP_SIZE_HEIGHT);
+                             if (fabs(pos(2)-HEIGHT) < TOL)
+                                current_state.armed = false;
+                        }      
+                        //Vector3f v = goal_pos;
+                        //ROS_INFO("GOAL=[%f, %f, %f], idx=%d", v(0), v(1), v(2), idx);
+                    }
+                    else if(traj_done)  goal_pos = landing[idx];
+                    else                goal_pos = waypoints[idx];
+                }
+                else                    goal_pos = takeoff;
+      
+                Vector3f v = goal_pos;
+                //ROS_INFO("GOAL2=[%f, %f, %f], idx=%d", v(0), v(1), v(2), idx);
                 local_pos_pub.publish(conversion_to_msg(goal_pos));
+            }
         }
         
         // Needed or your callbacks would never get called
@@ -237,6 +282,55 @@ ros::Subscriber APtag_est_pos_sub = nh.subscribe<geometry_msgs::PoseArray>
 
 
 // ------------------------------------------------------------------------------------------------
+Vector3f lands(Vector3f a, float H){
+    Vector3f v(0.0f, 0.0f, 0.0f);
+    v(0) = a(0);
+    v(1) = a(1);
+    v(2) = H-0.1f;
+    return v;
+}
+
+Vector3f landing_on_SP(Vector3f a, int id){
+    Vector3f v(0.0f, 0.0f, 0.0f);
+    if(id==0){
+        v(0) = a(0) - AP_SIZE/2.0f - SP_SIZE_WIDTH/2.0f;
+        v(1) = a(1) + AP_SIZE/2.0f + SP_SIZE_LENGTH/2.0f;
+        v(2) = a(2);
+    }
+    else if (id==1){
+        v(0) = a(0) - AP_SIZE/2.0f - SP_SIZE_LENGTH/2.0f;
+        v(1) = a(1) + AP_SIZE/2.0f + SP_SIZE_WIDTH/2.0f;
+        v(2) = a(2);
+    }
+    else if (id==2){
+        v(0) = a(0) + AP_SIZE/2.0f + SP_SIZE_LENGTH/2.0f;
+        v(1) = a(1) - AP_SIZE/2.0f + SP_SIZE_WIDTH/2.0f;
+        v(2) = a(2);
+    }
+    else
+        ROS_INFO("Not a defined tags");
+    return v;
+}
+
+Vector3f to_center_pose(Vector3f real_world, Vector3f camera_world, float offset_x, float offset_y){
+    Vector3f target_pos(0.0f, 0.0f, 0.0f);
+    target_pos(0) = real_world(0)-camera_world(1)+offset_y;
+    target_pos(1) = real_world(1)-camera_world(0)+offset_x;
+    target_pos(2) = camera_world(2);
+    return target_pos;
+}
+
+// To check is the drone is at the centered with the AP
+bool is_AP_centered(Vector3f a, float tol_x, float tol_y){
+    if( fabs(a(0)) < tol_x   &&   fabs(a(1)) < tol_y)   return true;
+    else                                                return false;                
+} 
+
+// To check if you found the right id
+bool check_id(int id){
+    if(AP_id == id)     return true;
+    else                return false;
+}
 
 
 // Callback which will save the current state of the autopilot
@@ -259,25 +353,52 @@ void est_local_pos_cb(const geometry_msgs::PoseStamped::ConstPtr& est_pos){
 //gazebo_msgs::ModelStates true_local_pos;
 void true_local_pos_cb(const gazebo_msgs::ModelStates::ConstPtr& true_pos){
     true_local_pos = *true_pos;
-//    ROS_INFO("True local pos=[%f, %f, %f]", true_pos->pose[2].position.x,
-//                                            true_pos->pose[2].position.y,
-//                                            true_pos->pose[2].position.z);
+    n_model = true_pos->pose.size();
+    //if(n_model>0){
+    //ROS_INFO("TRP =[%f, %f, %f]", true_pos->pose[5].position.x,
+    //                              true_pos->pose[5].position.y,
+    //                              true_pos->pose[5].position.z);
+    //}
 }
 
 // Callback which will save the estimated local position of the Apriltag
 //geometry_msgs::PoseArray APtag_est_pos;
-void APtag_est_pos_cb(const geometry_msgs::PoseArray::ConstPtr& AP_est_pos){
-    //ROS_INFO("CALLBACK AP");
-    APtag_est_pos = *AP_est_pos;
-    n_AP = AP_est_pos->poses.size();
-    //ROS_INFO("n=%d", n_AP);
-    //if(n_AP>0){  
-    //    ROS_INFO("APtag est pos=[%f, %f, %f]",  AP_est_pos->poses[0].position.x,
-    //                                            AP_est_pos->poses[0].position.y,
-    //                                            AP_est_pos->poses[0].position.z);
-    //}
-}
+//void APtag_est_pos_cb(const geometry_msgs::PoseArray::ConstPtr& AP_est_pos){
+//    APtag_est_pos = *AP_est_pos;
+//    n_AP = AP_est_pos->poses.size();
+//    //ROS_INFO("n=%d", n_AP);
+//    if(n_AP>0){
+//        AP_detected = true;
+//        //AP_in_verification = true;
+//            //ROS_INFO("APtag est pos=[%f, %f, %f]",  AP_est_pos->poses[0].position.x,
+//            //                                        AP_est_pos->poses[0].position.y,
+//            //                                        AP_est_pos->poses[0].position.z);
+//    }
+//    else
+//        AP_detected = false;
+//
+//}
 
+// Callback which will save the estimated local position of the Apriltag
+//geometry_msgs::PoseArray APtag_est_pos;
+void APtag_est_pos_cb(const apriltags_ros::AprilTagDetectionArray::ConstPtr& AP_est_pos){
+    APtag_est_pos = *AP_est_pos;
+    n_AP = AP_est_pos->detections.size();
+
+    //ROS_INFO("n=%d", n_AP);
+    if(n_AP>0){
+        AP_id = AP_est_pos->detections[0].id;
+        //ROS_INFO("ID=%d", AP_id);
+        AP_detected = true;
+        //AP_in_verification = true;
+            //ROS_INFO("APtag est pos=[%f, %f, %f]",  AP_est_pos->.detections[0].pose.pose.position.x,
+            //                                        AP_est_pos->.detections[0].pose.pose.position.y,
+            //                                        AP_est_pos->.detections[0].pose.pose.position.z);
+    }
+    else
+        AP_detected = false;
+
+}
 
 // To check is the drone is at the "right" place
 bool is_goal_reached(Vector3f a, Vector3f b, float tol)
